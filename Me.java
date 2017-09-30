9174d92c238f0dc24c7327f3d9ece8514f7d64aa
walthamstowbob <barry@leevalleyenergy.co.uk>
2017-09-30 13:25:04 +0200

    Take Me, Hire program

---
+++ Me.java
@@ -0,0 +1,14 @@
+public class Me {
+
+	public static void main(String[] args){
+
+		for(int i=1;i<=10;i++){
+
+			System.out.println("Me ..."+i);
+	
+		}
+
+	
+	}
+
+}
---
+++ TakeMeHire.java
@@ -0,0 +1,14 @@
+public class TakeMeHire {
+
+	public static void main(String[] args){
+
+		for(int i=1;i<=10;i++){
+
+			System.out.println("Take Me, Hire ..."+i);
+	
+		}
+
+	
+	}
+
+}
